3468c8b3124b54193e43b6e8e418372920b22e4b
Jacob-Petersen-1 <jacob.i.petersen52@gmail.com>
2022-03-30 12:32:23 -0600

    Create Game Controller for project + Create Get Game By ID + Create Get All Games + Test properly in postman

---
+++ ASP_NET_Video_Games_API/Controllers/GamesControllers.cs
@@ -0,0 +1,31 @@
+﻿using ASP_NET_Video_Games_API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_NET_Video_Games_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public GamesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllGames()
+        {
+            var videoGames = _context.VideoGames;
+            return Ok(videoGames);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetGamesById(int id)
+        {
+            var gameByID = _context.VideoGames.Where(x => x.Id == id).SingleOrDefault();
+            return Ok(gameByID);
+        }
+    }
+}
