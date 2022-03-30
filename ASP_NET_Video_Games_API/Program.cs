1f2b166274eccac0833b42d1cab285c3f6efabe6
matthew-pryor <pryor.matthew.a@gmail.com>
2022-03-30 10:25:06 -0500

    initialization of project in github

---
+++ ASP_NET_Video_Games_API/Program.cs
@@ -0,0 +1,33 @@
+using ASP_NET_Video_Games_API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(opts =>
+                   opts.UseSqlServer("server=(localdb)\\MSSQLLocalDB; database=video-games-database; Integrated Security=true",
+                   sqlServerOptions => sqlServerOptions.CommandTimeout(400)
+                   ));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
