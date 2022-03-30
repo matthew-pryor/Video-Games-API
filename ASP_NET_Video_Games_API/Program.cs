278a9b928b9bb4a88961ed269cfe16580e517d2c
Jacob-Petersen-1 <jacob.i.petersen52@gmail.com>
2022-03-30 14:06:03 -0600

    Add CORS middleware to interact with FrontEnd React project

--- ASP_NET_Video_Games_API/Program.cs
+++ ASP_NET_Video_Games_API/Program.cs
@@ -10,6 +10,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+    builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
 builder.Services.AddDbContext<ApplicationDbContext>(opts =>
                    opts.UseSqlServer("server=(localdb)\\MSSQLLocalDB; database=video-games-database; Integrated Security=true",
                    sqlServerOptions => sqlServerOptions.CommandTimeout(400)
